bdce998c315166fd813bc2267c391badd401ceaa
Luis Valdés Guerrero <lguerrero85@gmail.com>
2023-11-19 21:58:53 +0100

    Add lot of terminal utilities apps

--- home.nix
+++ home.nix
@@ -14,6 +14,7 @@
     curl
     tmux
     nnn
+    neofetch
     tree
     ncdu
     direnv
@@ -26,6 +27,59 @@
     mcfly # sh history navigator
     tldr # man
     hyperfine # command-line benchmarking tool
+    fzf # A command-line fuzzy finder
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
 
     # dev
     git
@@ -33,6 +87,8 @@
     jq # json cli tool
     httpie # curl
     curlie # curl
+    ripgrep # recursively searches directories for a regex pattern
+    yq-go # yaml processer https://github.com/mikefarah/yq
     gnumake
     emacs29
     # documentation
