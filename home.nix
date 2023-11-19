e093b6ed99862edf7f9086743bb28140b63459a8
Luis Valdés Guerrero <lguerrero85@gmail.com>
2023-11-19 22:05:43 +0100

    Add Starship shell prompt

--- home.nix
+++ home.nix
@@ -128,9 +128,19 @@
       includes = [
         { path = "~/Dropbox/config/.gitconfig"; }
       ];
-    };  
-  };
+    };
 
+    starship = {
+      enable = true;
+      # custom settings
+      settings = {
+        add_newline = false;
+        aws.disabled = true;
+        gcloud.disabled = true;
+        line_break.disabled = true;
+      };
+    };
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
